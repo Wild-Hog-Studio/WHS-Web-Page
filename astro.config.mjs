9d07a3c2e903b9d07cc517bd94769993ca86eee9
Yeriko <yerikoeko@gmail.com>
2025-05-09 01:23:05 -0600

    fix(vercel) added install astrojs vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-// @ts-check
 import 'dotenv/config';
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-
+import vercel from '@astrojs/vercel/serverless'; 
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+  adapter: vercel(), 
   vite: {
     plugins: [tailwindcss()]
   },
